0d6d2d192c9b14f8efd79a36c7dd91da1a87dc3d
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-04 22:38:06 +0300

    Created block customer feedback

---
+++ src/blocks/modules/customer-feedback/customer-feedback.js
@@ -0,0 +1,13 @@
+modules.define('customer-feedback', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});

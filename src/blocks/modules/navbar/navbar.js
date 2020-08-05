4c4c21ffa17ae44a3b25a0b455976a4b4980b144
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-05 21:42:50 +0300

    Added event for calling message window

--- src/blocks/modules/navbar/navbar.js
+++ src/blocks/modules/navbar/navbar.js
@@ -1,13 +1,13 @@
-modules.define('navbar', ['i-bem-dom'], function(provide, bemDom) {
+import $ from 'jquery';
+import Swal from 'sweetalert2';
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
-
-});
+// Event for warning
+$('#registration').on('click', function (event) {
+    Swal.fire({
+        title: 'В разработке',
+        text: 'Даты добавления неизвестна',
+        icon: 'info',
+        confirmButtonText: 'Закрыть',
+        footer: '<a href="#">info@richbee-relation.com</a>'
+    }).then(r => r);
+})
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
 import "%modules%/header/header";
+import "%modules%/navbar/navbar";
 import "%modules%/customer-feedback/customer-feedback";
 import "%modules%/footer/footer"
